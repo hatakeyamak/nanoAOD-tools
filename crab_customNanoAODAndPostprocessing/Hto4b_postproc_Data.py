7ce689660187b477a97cd823d6fbcb9e942843d6
Andrew Brinkerhoff <abrinke1@cern.ch>
2024-11-19 00:57:33 +0100

    AWB clean up data and MC postproc scripts

--- crab_customNanoAODAndPostprocessing/Hto4b_postproc_Data.py
+++ crab_customNanoAODAndPostprocessing/Hto4b_postproc_Data.py
@@ -13,7 +13,7 @@ import sys
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-IS_MC = True
+IS_MC = False
 YEAR  = '2018'
 runLocally = True
 
@@ -50,41 +50,11 @@ jmeCorrectionsAK4 = createJMECorrector(
     splitJER      = False,
     saveMETUncs   = ['T1', 'T1Smear'])
 
-## PU weights
-if YEAR == '2018':
-    puWeights = puWeight_UL2018
-elif YEAR == '2017':
-    puWeights = puWeight_UL2017
-elif YEAR == '2016':
-    puWeights = puWeight_UL2016
-
-## Prefiring corrections
-l1PrefirCorr2017 = lambda: PrefCorr(
-    jetroot="L1prefiring_jetpt_2017BtoF.root",
-    jetmapname="L1prefiring_jetpt_2017BtoF",
-    photonroot="L1prefiring_photonpt_2017BtoF.root",
-    photonmapname="L1prefiring_photonpt_2017BtoF",
-    branchnames=["PrefireWeight%s" % suff  for suff in ["","_Up","_Down"]],
-)
-
-# b-tag SF producer
-btagSF = lambda: btagSFProducer(
-    era = 'UL'+YEAR,
-    algo='deepjet', 
-    selectedWPs=['M'], # To include shapes: ['M', 'shape_corr']
-    sfFileName=None, 
-    verbose=0, 
-    jesSystsForShape=["jes"]
-)
-
-#fnames = ["/eos/cms/store/user/ssawant/NanoPost/SUSY_GluGluH_01J_HToAATo4B_Pt150_M-20_TuneCP5_13TeV_madgraph_pythia8/NanoTestPost/240921_092131/0000/PNet_v1_1.root"] 
-#fnames = ["/eos/cms/store/user/ssawant/NanoPost/SUSY_GluGluH_01J_HToAATo4B_Pt150_M-20_TuneCP5_13TeV_madgraph_pythia8/2017/2BFC55D0-269D-5045-8CF4-6174A5DEA5E7.root"] # 2017 sample
-fnames=["/afs/cern.ch/work/a/abrinke1/public/HiggsToAA/NanoAOD/crab/2018/CMSSW_10_6_30/src/PhysicsTools/NanoAOD/output/HtoAA_addHto4bPlus_ggH_HtoAA_MH-125_MA-32.5_Pt170_Eta2p4_Msoft10_Xbb0p6_skimFatCand_100k.root"]
-#fnames=["/afs/cern.ch/work/a/abrinke1/public/HiggsToAA/NanoAOD/crab/2018/CMSSW_10_6_30/src/PhysicsTools/NanoAOD/output/HtoAA_addHto4bPlus_ggHToBB_Pt-200toInf_Pt170_Eta2p4_Msoft10_Xbb0p6_skimFatCand_20k.root"]
+fnames=["/afs/cern.ch/work/a/abrinke1/public/HiggsToAA/NanoAOD/crab/2018/CMSSW_10_6_30/src/PhysicsTools/NanoAOD/output/HtoAA_addHto4bPlus_JetHT_2018C_Pt170_Eta2p4_Msoft10_Xbb0p6_skimFatCand_56k.root"]
 
 if not runLocally:
-    print("Hto4b_postproc_MC.py:: inputFiles() ", type(inputFiles()), ' : ',  inputFiles())
-    print("Hto4b_postproc_MC.py:: runsAndLumis ", type(runsAndLumis()), ' : ', runsAndLumis())
+    print("Hto4b_postproc_Data.py:: inputFiles() ", type(inputFiles()), ' : ',  inputFiles())
+    print("Hto4b_postproc_Data.py:: runsAndLumis ", type(runsAndLumis()), ' : ', runsAndLumis())
 
 p = PostProcessor(
     outputDir=".", 
@@ -92,12 +62,9 @@ p = PostProcessor(
     modules=[
         jmeCorrectionsAK8(),
         jmeCorrectionsAK4(),
-        puWeights(),
-        btagSF(),
-        Haa4bGenParticlesBranches(),
         Haa4bObjectSelectionProducer(IS_MC, YEAR)
-        ] + ([l1PrefirCorr2017()] if YEAR == '2017' else []), 
+        ]
     )
 p.run()
 
-print("DONE Hto4b_postproc_MC.py")
+print("DONE Hto4b_postproc_Data.py")
